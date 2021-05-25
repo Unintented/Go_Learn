d7a27c69722b63920a74540e87952a75e2256b25
Unintented <2058436124@qq.com>
2021-05-25 17:03:16 +0800

    总结了一些变量定义方式

--- _01Variable/variable.go
+++ _01Variable/variable.go
@@ -8,9 +8,22 @@ var v1 int
 var v2 string = "Kevin"
 var v3 = "曹操"
 
+//变量就是内存中的一块存储空间
+
+//var v int32  自动赋予类型初值
+//var v int64 = 10
+//var v1, v2 = "lotus",10
+/*
+var(
+v1 string
+v2 bool
+v3 interface{}
+v4 []int
+)
+*/
 func main() {
 	v1 = 18
-	//函数内定义变量的简便形式
+	//函数内定义变量的简便形式，也可以一次赋值多个变量
 	v4 := 5i
 	Println("age:", v1)
 	Println("English name:", v2)
